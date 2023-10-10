f17022e59dcd1af8fc3ce30f7ad0662065741ec5
Erkan_filiz <erkanfiliz1715@gmail.com>
2023-10-11 01:32:24 +0200

    image-gallery-apps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import "./style.css";
+import images from "./image";
 
 function App() {
+  const [selectedImg, setSelectedImg]=useState(images[0])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <div className="container">
+        <img src={selectedImg} alt="selected" className="selected"/>
+
+      </div>
+      <div className="imgContainer">
+        {images.map((img, index)=>(
+          <img key={index} src={img} alt="tantuni"
+          onClick={()=>setSelectedImg(img)}/>
+        ))}
+      </div>
+      </div>
   );
 }
 
